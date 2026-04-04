260983f30e819f6ed3c5c46e4dd84397c227c664
sovilleda07 <sovilleda07@gmail.com>
2026-04-03 21:37:52 -0600

    App Test

--- 04-multiplication/src/app.test.ts
+++ 04-multiplication/src/app.test.ts
@@ -1,7 +1,32 @@
-// import { describe, expect, it, test } from '@jest/globals';
+import { describe, expect, it, jest, test } from '@jest/globals';
+import { ServerApp } from './presentation/server-app';
 
-describe('App', () => {
-  test('Should be true', () => {
-    expect(true).toBe(true);
+describe('Test App.ts', () => {
+  test('Should call Server.run with values', async () => {
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+    process.argv = [
+      'node',
+      'app.ts',
+      '-b',
+      '10',
+      '-l',
+      '5',
+      '-s',
+      '-n',
+      'test-file',
+      '-d',
+      'test-destination',
+    ];
+
+    await import('./app');
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 5,
+      showTable: true,
+      fileName: 'test-file',
+      fileDestination: 'test-destination',
+    });
   });
 });
