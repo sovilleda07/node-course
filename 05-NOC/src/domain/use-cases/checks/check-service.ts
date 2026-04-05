2fa1d852c611809018ab47778b45c8a2e625ef9b
sovilleda07 <sovilleda07@gmail.com>
2026-04-04 22:26:24 -0600

    Dependency injection in Check-Service

--- 05-NOC/src/domain/use-cases/checks/check-service.ts
+++ 05-NOC/src/domain/use-cases/checks/check-service.ts
@@ -2,7 +2,20 @@ interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
 
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
 export class CheckService implements CheckServiceUseCase {
+  // Inyección de dependencias (Añadirle dependencias a la clase)
+  // no es más que colocar una dependencia
+  // en nuestros casos de usos o en nuestros repositorios o data sources
+  // o en cualquier parte de nuestra apliación.
+  // En JS se realiza con un Factory Function
+  constructor(
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback,
+  ) {}
+
   // Public porque se necesita hacer una inyección.
   public async execute(url: string): Promise<boolean> {
     try {
@@ -12,11 +25,12 @@ export class CheckService implements CheckServiceUseCase {
         throw new Error(`Error on check service ${url}`);
       }
 
-      console.log(`${url} is ok`);
+      this.successCallback();
       return true;
     } catch (error) {
       console.log(`${error}`);
 
+      this.errorCallback(`${error}`);
       return false;
     }
   }
