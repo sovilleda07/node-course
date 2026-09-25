b284ba5c59a3b058107297e1fc27f53654c39b9a
sovilleda07 <sovilleda07@gmail.com>
2026-09-24 21:25:46 -0600

    Restful API

--- 07-RESTWEB/presentation/server.ts
+++ 07-RESTWEB/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
   port: number;
+  routes: Router;
   public_path?: string;
 }
 
@@ -11,18 +12,29 @@ export class Server {
 
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port, public_path = 'public' } = options;
+    const { port, routes, public_path = 'public' } = options;
     this.port = port;
     this.publicPath = public_path;
+    this.routes = routes;
   }
 
   async start() {
-    // Middlewares: funciones que se ejecutan en todo momento que pase una ruta
+    //* Middlewares: funciones que se ejecutan en todo momento que pase una ruta
+    // Serealiza el body como json
+    this.app.use(express.json()); // raw
+    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
+
+    //* Public Folder
     // Public folder
     this.app.use(express.static(this.publicPath));
 
+    //* Routes
+    this.app.use(this.routes);
+
+    //*SPA
     this.app.get('/*filepath', (req, res) => {
       const indexPath = path.join(
         __dirname + `../../../${this.publicPath}/index.html`,
